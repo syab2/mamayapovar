e4a82fce80a8803ecc65eb482468025581adb8e9
lostsleepyboii <lostsleepyboii@gmail.com>
2022-11-23 01:25:08 +0500

    стартуем

---
+++ src/js/_components.js
@@ -0,0 +1,2 @@
+import './components/_menu-profile.js';
+import './components/_modal.js';
---
+++ src/js/components/_menu-profile.js
@@ -0,0 +1,36 @@
+(function(){
+  const menuProfileToggle = document?.querySelector('[data-menu-profile-toggle]');
+  const menuProfile = document?.querySelector('[data-menu-profile]');
+  const menuProfileLinks = document?.querySelectorAll('[data-menu-profile-link]');
+
+  if (menuProfileToggle) {
+    menuProfileToggle?.addEventListener('click', (e) => {
+      menuProfile?.classList.toggle('active');
+
+      if (menuProfile?.classList.contains('active')) {
+        menuProfileToggle?.setAttribute('aria-expanded', 'true');
+        menuProfileToggle?.setAttribute('aria-label', 'Закрыть меню');
+      } else {
+        menuProfileToggle?.setAttribute('aria-expanded', 'false');
+        menuProfileToggle?.setAttribute('aria-label', 'Открыть меню');
+      }
+    });
+
+    window.addEventListener('click', (e) => {
+      const target = e.target
+      if (!target.closest('[data-menu-profile-toggle]') && !target.closest('[data-menu-profile]')) {
+        menuProfileToggle?.setAttribute('aria-expanded', 'false');
+        menuProfileToggle?.setAttribute('aria-label', 'Открыть меню');
+        menuProfile.classList.remove('active');
+      }
+    });
+
+    menuProfileLinks?.forEach(el => {
+      el.addEventListener('click', () => {
+        menuProfileToggle?.setAttribute('aria-expanded', 'false');
+        menuProfileToggle?.setAttribute('aria-label', 'Открыть меню');
+        menuProfile.classList.remove('active');
+      });
+    });
+  }
+})();
