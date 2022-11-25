088a4f936f8f9a0327702de2ea56d953c553b006
Alexei Kalinin <72299738+sonniydsgn@users.noreply.github.com>
2022-11-25 21:13:03 +0500

    починил поля + добавил рандомный айди

--- mamayapovar/recipes/static/recipes/js/main.js
+++ mamayapovar/recipes/static/recipes/js/main.js
@@ -243,50 +243,49 @@ if (_vars__WEBPACK_IMPORTED_MODULE_0__["default"].bodyEl.querySelector('.ingredi
   (function () {
     const ingredientSection = document.querySelector('.ingredient');
     const ingredientList = ingredientSection.querySelector('.ingredient__list');
+    const ingredientAdd = ingredientSection.querySelector('.ingredient__btn');
     let countOfFields = 2;
-    let fieldIndex = 1;
-    ingredientSection.addEventListener('click', sec => {
-      sec.preventDefault();
-
-      // добавление
-      if (sec.target.classList.contains('ingredient__btn')) {
-        countOfFields++;
-        fieldIndex++;
-        ingredientList.innerHTML += `
-					<div class="ingredient-item" id="ingredient-${fieldIndex}">
-						<input type="text" name="ingredient-name-${fieldIndex}" class="input  ingredient-item__input  ingredient-item__input--name" placeholder="Название ингредиента" autocomplete="off" required>
-						<input type="number" name="ingredient-amount-${fieldIndex}" class="input  ingredient-item__input  ingredient-item__input--amount" value="1" min="1" max="5000" autocomplete="off" required>
-						<div class="select  ingredient-item__select">
-							<select name="ingredient-measure-${fieldIndex}" aria-label="Единица измерения" required>
-								<option value="">Ед. измерения</option>
-								<option>шт</option>
-								<option>г</option>
-								<option>кг</option>
-								<option>мл</option>
-								<option>л</option>
-								<option>столовая ложка</option>
-								<option>чайная ложка</option>
-								<option>стакан</option>
-							</select>
-							<svg class="icon  select__icon" aria-hidden="true" focusable="false">
-								<use href="img/sprite.svg#chevron-down"/>
-							</svg>
-						</div>
-						<a class="btn-reset  ingredient-item__delete" aria-label="Удалить ингредиент">
-							<svg class="icon  icon--16  ingredient-item__icon" aria-hidden="true" focusable="false">
-								<use href="img/sprite.svg#cross" />
-							</svg>
-						</a>
-					</div>
-				`;
-      }
-
-      // удаление
-      if (sec.target.classList.contains('ingredient-item__delete') && countOfFields > 1) {
+    function randomID() {
+      return Math.floor(Math.random() * Date.now());
+    }
+    ingredientList.addEventListener('click', e => {
+      let target = e.target;
+      if (target.classList.contains('ingredient-item__delete') && countOfFields > 1) {
         countOfFields--;
-        fieldIndex--;
-        sec.target.parentElement.remove();
+        target.parentNode.remove();
       }
+    });
+    ingredientAdd.addEventListener('click', e => {
+      e.preventDefault();
+      countOfFields++;
+      const fieldIndex = randomID();
+      ingredientList.innerHTML += `
+				<div class="ingredient-item" id="ingredient-${fieldIndex}">
+					<input type="text" name="ingredient-name-${fieldIndex}" class="input  ingredient-item__input  ingredient-item__input--name" placeholder="Название ингредиента" autocomplete="off" required>
+					<input type="number" name="ingredient-amount-${fieldIndex}" class="input  ingredient-item__input  ingredient-item__input--amount" value="1" min="1" max="5000" autocomplete="off" required>
+					<div class="select  ingredient-item__select">
+						<select name="ingredient-measure-${fieldIndex}" aria-label="Единица измерения" required>
+							<option value="">Ед. измерения</option>
+							<option>шт</option>
+							<option>г</option>
+							<option>кг</option>
+							<option>мл</option>
+							<option>л</option>
+							<option>столовая ложка</option>
+							<option>чайная ложка</option>
+							<option>стакан</option>
+						</select>
+						<svg class="icon  select__icon" aria-hidden="true" focusable="false">
+							<use href="img/sprite.svg#chevron-down"/>
+						</svg>
+					</div>
+					<a class="btn-reset  ingredient-item__delete" aria-label="Удалить ингредиент"  role="button">
+						<svg class="icon  icon--16  ingredient-item__icon" aria-hidden="true" focusable="false">
+							<use href="img/sprite.svg#cross" />
+						</svg>
+					</a>
+				</div>
+			`;
     });
   })();
 }
@@ -296,54 +295,51 @@ if (_vars__WEBPACK_IMPORTED_MODULE_0__["default"].bodyEl.querySelector('.step'))
   (function () {
     const stepSection = document.querySelector('.step');
     const stepList = stepSection.querySelector('.step__list');
+    const stepAdd = stepSection.querySelector('.step__btn');
     let countOfFields = 2;
-    let fieldIndex = 1;
-    stepSection.addEventListener('click', sec => {
-      sec.preventDefault();
-
-      // добавление
-      if (sec.target.classList.contains('step__btn')) {
-        countOfFields++;
-        fieldIndex++;
-        stepList.innerHTML += `
-					<div class="step-item" id="step-${fieldIndex}">
-					<div class="step-item__header">
-						<span class="input__label  step-item__number"></span>
-						<a href="#" class="btn-reset  step-item__delete" aria-label="Удалить шаг">
-							<svg class="icon  step-item__icon" aria-hidden="true" focusable="false">
-								<use href="img/sprite.svg#cross" />
+    function randomID() {
+      return Math.floor(Math.random() * Date.now());
+    }
+    stepList.addEventListener('click', e => {
+      let target = e.target;
+      if (target.classList.contains('step-item__delete') && countOfFields > 1) {
+        countOfFields--;
+        target.parentNode.remove();
+      }
+    });
+    stepAdd.addEventListener('click', e => {
+      e.preventDefault();
+      countOfFields++;
+      const fieldIndex = randomID();
+      stepList.innerHTML += `
+				<div class="step-item" id="step-${fieldIndex}">
+				<span class="input__label  step-item__number"></span>
+				<a class="btn-reset  step-item__delete" aria-label="Удалить шаг"  role="button">
+					<svg class="icon  step-item__icon" aria-hidden="true" focusable="false">
+						<use href="img/sprite.svg#cross" />
+					</svg>
+				</a>
+				<div class="step-item__body">
+					<div class="imageuploader  step-item__imageuploader">
+						<label class="input  input--photo  imageuploader__input">
+							<input type="file" name="step-photo-${fieldIndex}" accept=".jpg, .jpeg, .png" required>
+							<svg class="icon  input__icon" aria-hidden="true" focusable="false">
+								<use href="img/sprite.svg#image"/>
+							</svg>
+							Загрузите фото блюда
+						</label>
+						<a class="btn  btn--other  imageuploader__btn  hidden">
+							<svg class="icon  icon--16  btn__icon" aria-hidden="true" focusable="false">
+								<use href="img/sprite.svg#delete" />
 							</svg>
 						</a>
 					</div>
-					<div class="step-item__body">
-						<div class="imageuploader  step-item__imageuploader">
-							<label class="input  input--photo  imageuploader__input">
-								<input type="file" name="step-photo-${fieldIndex}" accept=".jpg, .jpeg, .png" required>
-								<svg class="icon  input__icon" aria-hidden="true" focusable="false">
-									<use href="img/sprite.svg#image"/>
-								</svg>
-								Загрузите фото блюда
-							</label>
-							<a class="btn  btn--other  imageuploader__btn  hidden">
-								<svg class="icon  icon--16  btn__icon" aria-hidden="true" focusable="false">
-									<use href="img/sprite.svg#delete" />
-								</svg>
-							</a>
-						</div>
-						<textarea name="step-description-${fieldIndex}" class="input  input--textarea  step-item__input"
-							placeholder="Замешиваем тесто для блинов. В 1 литр теплого молока добавляем 4 яйца..."
-							autocomplete="off" maxlength="5000" required></textarea>
-					</div>
+					<textarea name="step-description-${fieldIndex}" class="input  input--textarea  step-item__input"
+						placeholder="Замешиваем тесто для блинов. В 1 литр теплого молока добавляем 4 яйца..."
+						autocomplete="off" maxlength="5000" required></textarea>
 				</div>
-				`;
-      }
-
-      // удаление
-      if (sec.target.classList.contains('step-item__delete') && countOfFields > 1) {
-        countOfFields--;
-        fieldIndex--;
-        sec.target.parentElement.parentElement.remove();
-      }
+			</div>
+			`;
     });
   })();
 }
