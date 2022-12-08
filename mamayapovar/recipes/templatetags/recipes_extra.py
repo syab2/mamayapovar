861c2ab6dd2e1606dd16ca40a898b021793ce747
Крылов Николай <81432691+syab2@users.noreply.github.com>
2022-12-08 21:50:23 +0300

    Update recipes_extra.py

--- mamayapovar/recipes/templatetags/recipes_extra.py
+++ mamayapovar/recipes/templatetags/recipes_extra.py
@@ -1,5 +1,6 @@
 from django import template
-from recipes.models import Bookmark
+from django.contrib.auth.models import User
+from recipes.models import Bookmark, UserProfile, Like, Subscribe, Recipe
 
 register = template.Library()
 
@@ -10,3 +11,70 @@ def get_bookmark_status(id_):
         return True if Bookmark.objects.get(book_post_id=id_) else False
     except:
         return False
+
+
+@register.simple_tag
+def is_avatar(id_):
+    try:
+        return True if UserProfile.objects.get(user=id_) else False
+    except:
+        return False
+
+
+@register.simple_tag
+def get_avatar_url(id_):
+    return UserProfile.objects.get(user_id=id_).avatar.url if UserProfile.objects.filter(user_id=id_) else None
+
+
+@register.simple_tag
+def get_likes_count(id_):
+    return sum([x.likes for x in Like.objects.filter(like_post_id=id_)])
+
+
+@register.simple_tag
+def get_likes_count_of_user(id_):
+    recipes = Recipe.objects.filter(author_id=id_)
+    try:
+        likes = 0
+        for elem in recipes:
+            for el in Like.objects.filter(like_post=elem):
+                likes += el.likes
+        return likes
+    except:
+        return 0
+
+
+@register.simple_tag
+def get_likes_status(id_, user_id):
+    try:
+        return Like.objects.get(like_post_id=id_, like_user_id=user_id).status
+    except:
+        return False
+
+
+@register.simple_tag
+def get_sub_status(id1, id2):
+    return True if Subscribe.objects.filter(subscribe_from_id=id1, subscribe_to_id=id2) else False
+
+
+@register.simple_tag
+def get_all_subscribers(id_):
+    subs = Subscribe.objects.filter(subscribe_from_id=id_)
+    new_subs = []
+    for elem in subs:
+        new_subs.append(User.objects.get(id=elem.subscribe_to_id))
+    return new_subs
+
+
+@register.simple_tag
+def get_count_of_subscribers(id_):
+    subs = Subscribe.objects.filter(subscribe_to_id=id_)
+    new_subs = []
+    for elem in subs:
+        new_subs.append(User.objects.get(id=elem.subscribe_from_id))
+    return len(new_subs)
+
+
+@register.simple_tag
+def get_correct_path(id_):
+    return f'user/{id_}'
