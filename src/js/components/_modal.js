4a77cd1277b5784eeb1da09804c7181991610a8b
lostsleepyboii <lostsleepyboii@gmail.com>
2022-12-19 23:51:59 +0500

    фикс багов модальных окон

--- src/js/components/_modal.js
+++ src/js/components/_modal.js
@@ -1,2 +1,19 @@
 import GraphModal from 'graph-modal';
-const modal = new GraphModal();
+
+const modal = new GraphModal({
+	isOpen: (modal) => {
+		const registerName = document.querySelector('#username-register')
+		const authEmail = document.querySelector('#email-auth')
+
+		if (registerName) {
+			setTimeout(function(){
+				registerName.focus()
+			}, 100);
+		}
+		if (authEmail) {
+			setTimeout(function(){
+				authEmail.focus()
+			}, 100);
+		}
+	},
+});
